a7904c436d76a6f10411c85b3b3a321715d22a61
tpstjn <63107315+tpstjn@users.noreply.github.com>
2023-03-24 21:05:27 -0400

    Didn't do great, but it's "finished"

--- test/README.txt
+++ test/README.txt
@@ -1,1 +1,1 @@
-Put your JUnit test classes and test doubles in this folder.
+Thomas St.Jean & Amy Cunningham
--- test/StatsFileTest.java
+++ test/StatsFileTest.java
@@ -1,3 +1,47 @@
-public class StatsFileTest {
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatsFileTest {
+    //Using dependency injection
+    @Test
+    void testInitStatsReader() throws FileNotFoundException {
+        StatsFile sFile = new StatsFile(new CSVReaderDummy());
+        assertEquals(1, sFile.numGames(3));
+    }
+}
+
+class CSVReaderDummy extends CSVReader {
+    private boolean read;
+    CSVReaderDummy() throws FileNotFoundException {
+        //Doesn't matter what we do with this
+        super(new FileReader("./test/test.txt"));
+        read = true;
+    }
+
+    //Only method that actually needs to do something
+    @Override
+    public String[] readNext() {
+        if(read) {
+            read = false;
+            LocalDateTime time = LocalDateTime.now();
+            return new String[]{time.toString(), "3"};
+        }
+        read = true;
+        return null;
+    }
+
+    //Do nothing
+    @Override
+    public void forEach(Consumer<? super String[]> action) {
+
+    }
 }
